26fd7b8d1ed2214948d4cb1c688599db68a94499
Ítalo Barbosa Santos <italo.b.s.35@outlook.com>
2022-03-05 04:19:23 +0900

    Adiciona primeiros testes com python

---
+++ treinamento-python/introducao-python/aula01.py
@@ -0,0 +1,5 @@
+print("meu primeiro programa Python")
+a = 2
+b = 3
+soma = a + b
+print(soma)
---
+++ treinamento-python/introducao-python/aula02.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+
+print('Soma: {}'.format(soma))
+print('Subtração: {}'.format(subtracao))
+print('Multiplicação: {}'.format(multiplicacao))
+print('Divisão: {}'.format(divisao))
+print('Resto: {}'.format(resto))
+
